4ce2adf83eda1fcc8d49e8dc780ba87c8b66c4fa
Radu Chiriac <thewhiz84@gmail.com>
2022-01-15 15:27:44 +0200

    Added instructor alternative to Day 19 - Turtle Etch-A-Sketch game

--- Day19-Etch_a_Sketch_and_Turtle_Race/etch_a_sketch.py
+++ Day19-Etch_a_Sketch_and_Turtle_Race/etch_a_sketch.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 tim.shape("turtle")
-angle = 0
+# angle = 0
 
 
 def move_forward():
@@ -14,29 +14,36 @@ def move_backward():
     tim.bk(10)
 
 
-def rotate_clockwise():
-    global angle
-    angle -= 10
-    tim.setheading(angle)
+# def turn_right():
+#     global angle
+#     angle -= 10
+#     tim.setheading(angle)
+#Alternative ways to achieve the same:
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
 
-
-def rotate_counter_clockwise():
-    global angle
-    angle += 10
-    tim.setheading(angle)
+def turn_left():
+    tim.left(10)
 
 
 def reset():
     tim.reset()
-
+# #An alternative that is longer but keeps the turtle parameters like color and size
+# def reset():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+# tim.color("green")
 
 screen.listen()
 
 def etch_a_sketch():
     screen.onkey(fun=move_forward, key="w")
     screen.onkey(fun=move_backward, key="s")
-    screen.onkey(fun=rotate_counter_clockwise, key="a")
-    screen.onkey(fun=rotate_clockwise, key="d")
+    screen.onkey(fun=turn_left, key="a")
+    screen.onkey(fun=turn_right, key="d")
     screen.onkey(fun=reset, key="c")
 
 etch_a_sketch()
