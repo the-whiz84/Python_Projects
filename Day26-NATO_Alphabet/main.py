9bddb36e51a3284d9f3abdbbe6f7d24a704a62d5
Radu Chiriac <thewhiz84@gmail.com>
2022-02-06 16:12:57 +0200

    Added alternative method in Day 26 project

--- Day26-NATO_Alphabet/main.py
+++ Day26-NATO_Alphabet/main.py
@@ -1,22 +1,25 @@
+from operator import index
 import pandas
 
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 
-with open("./nato_phonetic_alphabet.csv") as data_file:
-    alphabet_df = pandas.read_csv(data_file)
+data_frame = pandas.read_csv("./nato_phonetic_alphabet.csv")
+nato_dict = {row.letter:row.code for (index, row) in data_frame.iterrows()}
 
-nato_dict = {row.letter:row.code for (index, row) in alphabet_df.iterrows()}
+# Alternative way to create the dictionary using built in methods set_index() and to_dict()
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.set_index.html
+
+# nato_dict = data_frame.set_index("letter")["code"].to_dict()
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# Initial code
 
+# Initial code
 # user_word = input("Please type a word to get the NATO phonetic spelling:\n")
 # word_letters = [letter.upper() for letter in user_word]
 # nato_letters = [nato_dict[letter] for letter in word_letters]
 
 # Optimized code
-
 user_word = input("Please type a word to get the NATO phonetic spelling:\n").upper()
 nato_letters = [nato_dict[letter] for letter in user_word]
 
