a8a5bfb4158d40c17ba3b83cd5978b8feaf12401
Radu Chiriac <radu@zerodawn.tech>
2023-12-03 13:45:51 +0200

    Updated

--- Day04-Rock_Paper_Scissors/rps.py
+++ Day04-Rock_Paper_Scissors/rps.py
@@ -31,45 +31,50 @@ import random
 
 # From there you will need to figure out: 
 # * How you will store the user's input.
+game_images = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if player_choice == 0:
-    print(rock)
-elif player_choice == 1:
-    print(paper)
-elif player_choice == 2:
-    print(scissors)
-else:
-    print("Invalid input! Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
-
-
-# * How you will generate a random choice for the computer.
-ai_choice = random.randint(0, 2)
-
-print("Computer chose:\n")
-if ai_choice == 0:
-    print(rock)
-elif ai_choice == 1:
-    print(paper)
-elif ai_choice == 2:
-    print(scissors)
-
-
-
-# * How you will compare the user's and the computer's choice to determine the winner (or a draw).
 if player_choice >= 3 or player_choice < 0:
     print("You chose an invalid option, please try again!")
-elif player_choice == ai_choice:
-    print("It's a draw, try again!\n")
-elif player_choice == 0 and ai_choice == 2:
-    print("You win!")
-elif player_choice == 2 and ai_choice == 0:
-    print("You lose!")
-elif player_choice < ai_choice:
-    print("You lose!")
 else:
-    print("You win!")
+    print(game_images[player_choice])
+    # if player_choice == 0:
+    #     print(rock)
+    # elif player_choice == 1:
+    #     print(paper)
+    # elif player_choice == 2:
+    #     print(scissors)
+    # else:
+    #     print("Invalid input! Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+
+    # * How you will generate a random choice for the computer.
+    ai_choice = random.randint(0, 2)
+
+    print("Computer chose:")
+    print(game_images[ai_choice])
+    # if ai_choice == 0:
+    #     print(rock)
+    # elif ai_choice == 1:
+    #     print(paper)
+    # elif ai_choice == 2:
+    #     print(scissors)
+
+
+
+    # * How you will compare the user's and the computer's choice to determine the winner (or a draw).
+
+    if player_choice == ai_choice:
+        print("It's a draw, try again!\n")
+    elif player_choice == 0 and ai_choice == 2:
+        print("You win!")
+    elif player_choice == 2 and ai_choice == 0:
+        print("You lose!")
+    elif player_choice < ai_choice:
+        print("You lose!")
+    else:
+        print("You win!")
 
 
 
