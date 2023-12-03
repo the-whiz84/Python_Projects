3bcaedec8657d9e614f0b815c7ccc87ccd84c5fa
Radu Chiriac <radu@zerodawn.tech>
2023-12-03 13:45:51 +0200

    Updated

--- Day04-Rock_Paper_Scissors/rps.py
+++ Day04-Rock_Paper_Scissors/rps.py
@@ -31,29 +31,32 @@ import random
 
 # From there you will need to figure out: 
 # * How you will store the user's input.
+game_images = [rock, paper, scissors]
 
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if player_choice == 0:
-    print(rock)
-elif player_choice == 1:
-    print(paper)
-elif player_choice == 2:
-    print(scissors)
-else:
-    print("Invalid input! Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+print(game_images[player_choice])
+# if player_choice == 0:
+#     print(rock)
+# elif player_choice == 1:
+#     print(paper)
+# elif player_choice == 2:
+#     print(scissors)
+# else:
+#     print("Invalid input! Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
 
 # * How you will generate a random choice for the computer.
 ai_choice = random.randint(0, 2)
 
 print("Computer chose:\n")
-if ai_choice == 0:
-    print(rock)
-elif ai_choice == 1:
-    print(paper)
-elif ai_choice == 2:
-    print(scissors)
+print(game_images[ai_choice])
+# if ai_choice == 0:
+#     print(rock)
+# elif ai_choice == 1:
+#     print(paper)
+# elif ai_choice == 2:
+#     print(scissors)
 
 
 
