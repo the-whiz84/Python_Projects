e9d81bd819b72515880fdca9b9e9706ec1a19519
Radu Chiriac <radu@zerodawn.tech>
2024-08-21 22:48:14 +0300

    Updated Day 18 - Hirst Painting

--- Day18-The_Hirst_Painting/main.py
+++ Day18-The_Hirst_Painting/main.py
@@ -1,50 +1,48 @@
-# PART 1 - How to extract RGB values from images
+# Use pip to install colorgram module
+
 # import colorgram
-
+#
 # rgb_colors = []
-# colors = colorgram.extract('Day18-The_Hirst_Painting/image.jpg', 30)
-
+# colors = colorgram.extract('image.jpg', 30)
 # for color in colors:
 #     r = color.rgb.r
 #     g = color.rgb.g
 #     b = color.rgb.b
 #     new_color = (r, g, b)
 #     rgb_colors.append(new_color)
-
+#
 # print(rgb_colors)
 
-from data import rgb_colors
-
-# PART 2 - Drawing the Dots
-# The turtle must draw a grid of 10 dots horizontally and 10 dots vertically
-# Dots size is 20, space between dots is 50
 import random
-from turtle import Turtle, Screen, colormode
+import turtle as turtle_module
 
-timmy = Turtle()
-timmy.speed("fast")
-colormode(255)
-timmy.hideturtle()
+turtle_module.colormode(255)
 
-x = -225
-y = -225
+color_list = [(23, 16, 94), (232, 43, 6), (153, 14, 30), (41, 181, 158), (127, 253, 206), (237, 71, 166), (209, 179, 208), (246, 218, 21),
+              (40, 133, 242), (244, 247, 253), (246, 218, 5), (207, 148, 178), (126, 155, 204), (106, 189, 174), (224, 134, 117), (81, 87, 136),
+              (150, 64, 75), (209, 87, 66), (49, 44, 100), (244, 168, 154), (175, 184, 222), (111, 9, 23), (179, 30, 10)]
 
-def draw_dots(y_coordinate):
-    timmy.penup()
-    timmy.setpos(x, y)
-    timmy.pendown()
-    for _ in range(10):
-        color = random.choice(rgb_colors)
+
+def draw_dots():
+    for item in range(10):
+        color = random.choice(color_list)
         timmy.dot(20, color)
-        timmy.penup()
-        timmy.fd(50)
+        timmy.forward(50)
 
 
-for _ in range(10):
-    draw_dots(y)
-    y += 50
-    timmy.setpos(x, y)
+timmy = turtle_module.Turtle()
+timmy.penup()
+timmy.hideturtle()
+timmy.speed("fast")
+x_pos = -250
+y_pos = -250
+timmy.teleport(x_pos, y_pos)
+
+for pos in range(10):
+    y_pos += 50
+    timmy.teleport(x_pos, y_pos)
+    draw_dots()
 
 
-screen = Screen()
+screen = turtle_module.Screen()
 screen.exitonclick()
