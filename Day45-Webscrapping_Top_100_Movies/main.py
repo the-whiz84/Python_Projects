45d3c1e86e5875439e09ede8e1801689d9a4673a
Radu Chiriac <radu@zerodawn.tech>
2024-09-10 19:27:31 +0300

    Added Day 45 - Webscrapping project

---
+++ Day45-Webscrapping_Top_100_Movies/main.py
@@ -0,0 +1,44 @@
+
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+ 
+WEB_PAGE = "https://www.empireonline.com/movies/features/best-movies-2/"
+WEB_FILE = "./data/100_best_movies.html"
+ 
+# Using requests_html to render JavaScript
+def get_web_page():
+    # create an HTML Session object
+    session = HTMLSession()
+    # Use the object above to connect to needed webpage
+    response = session.get(WEB_PAGE)
+    # Run JavaScript code on webpage
+    response.html.render()
+ 
+    # Save web page to file
+    with open(WEB_FILE, mode="w", encoding="utf-8") as fp:
+        fp.write(response.html.html)
+ 
+def read_web_file():
+    try:
+        open(WEB_FILE)
+    except FileNotFoundError:
+        get_web_page()
+    finally:
+        # Read the web page from file
+        with open(WEB_FILE, mode="r", encoding="utf-8") as fp:
+            content = fp.read()
+        return BeautifulSoup(content, "html.parser")
+ 
+# Read web file if it exists, load from internet if it doesn't exist
+soup = read_web_file()
+
+movies = []
+all_movies = soup.find_all(name="h3", class_="listicleItem_listicle-item__title__BfenH")
+
+for movie in all_movies:
+    title = movie.getText()
+    movies.append(title)
+
+with open("./movies.txt", "w") as file:
+    for movie in movies[::-1]:
+        file.write(f"{movie}\n")
